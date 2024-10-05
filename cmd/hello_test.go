fd6728e29bae6bccd44c0ee058ab3a1182b5aa46
Alex Martishin <asmartishin@gmail.com>
2024-10-05 19:50:07 +0200

    finished with the hello world example

--- cmd/hello_test.go
+++ cmd/hello_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Alex")
+		got := Hello("Alex", "")
 		want := "Hello, Alex"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Louis", "French")
+		want := "Bonjour, Louis"
 		assertCorrectMessage(t, got, want)
 	})
 }
